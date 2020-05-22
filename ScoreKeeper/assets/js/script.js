e4dd45e96d1ef0b903327334bd68a60a908f83a5
Avantika184001 <54210563+Avantika184001@users.noreply.github.com>
2020-05-22 10:06:10 +0530

    Added new project

---
+++ ScoreKeeper/assets/js/script.js
@@ -0,0 +1,8 @@
+var ad=prompt("Whats your first name?");
+var sd=prompt("Whats your last name?");
+var df=prompt("Whats your age?");
+
+console.log("your name is"+" "+ad+" "+sd+".")
+console.log("your age is"+" "+df+"years old.")
+
+
